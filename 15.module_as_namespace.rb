f2f0e33d06d1bac563374172ce80548b74e18a3d
Seangel.Yu <Seangel_yuxh@hotmail.com>
2014-05-21 14:31:00 +0800

    15 start

---
+++ 15.module_as_namespace.rb
@@ -0,0 +1,15 @@
+# use module as name space.
+#---------------------
+# Author: Seangel.Yu
+# Copyright 2014 Seangel.Yu
+#
+# Document: An example Ruby class
+#
+
+# 
+#---------------------
+# How to use:
+#--------------------------
+# use module to orginaze class constants and moudles.
+#
+#
