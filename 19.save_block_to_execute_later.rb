0dbc7463d31af309f31a322dc2eafbe7994bce7c
Graphy-China <Seangel_yuxh@hotmail.com>
2014-05-23 21:24:40 +0800

    19.begin

---
+++ 19.save_block_to_execute_later.rb
@@ -0,0 +1,15 @@
+# 18. using blocks to transport your code through time.
+#---------------------
+# Author: Seangel.Yu
+# Copyright 2014 Seangel.Yu
+#
+# Document: An example Ruby class
+#
+
+# 
+#---------------------
+# How to use:
+#--------------------------
+# 
+
+
