f66cf813d9a042d3c9bd71efb37df5c8728dedbd
Seangel.Yu <Seangel_yuxh@hotmail.com>
2014-06-05 10:58:09 +0800

    new test demo

---
+++ testdemo/app/controllers/events_controller.rb
@@ -0,0 +1,44 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    #TBD.
+    @event = Event.new(params[:event])
+    @event.save
+    #render :@event.name
+
+    redirect_to events_url
+    #redirect_to  :action => :index
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update_attributes(param[:evebt])
+    @event.save
+    
+    redirect_to event_url(@event)
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    
+    redirect_to :action => :index
+  end
+
+
+end
