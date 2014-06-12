71267fcf8eccc3c95eb447d4117205428bc2fef9
Seangel.Yu <Seangel_yuxh@hotmail.com>
2014-06-12 09:18:18 +0800

    Add CURD Ajax

---
+++ AjaxCURD/AddressBook/app/controllers/entries_controller.rb
@@ -0,0 +1,105 @@
+class EntriesController < ApplicationController
+  before_action :set_entry, only: [:show, :edit, :update, :destroy]
+
+  # GET /entries
+  # GET /entries.json
+  def index
+    @entries = Entry.all
+    @entry = Entry.new
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.js
+    end
+
+  end
+
+  # GET /entries/1
+  # GET /entries/1.json
+  def show
+    @entry = Entry.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.js
+    end
+
+  end
+
+  # GET /entries/new
+  def new
+    @entry = Entry.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.js
+    end
+
+  end
+
+  # GET /entries/1/edit
+  def edit
+    @entry = Entry.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  # POST /entries
+  # POST /entries.json
+  def create
+    @entry = Entry.new(entry_params)
+
+    respond_to do |format|
+      if @entry.save
+        format.html { redirect_to @entry, notice: 'Entry was successfully created.' }
+        format.json { render :show, status: :created, location: @entry }
+        format.js
+      else
+        format.html { render :new }
+        format.json { render json: @entry.errors, status: :unprocessable_entity }
+        format.js { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /entries/1
+  # PATCH/PUT /entries/1.json
+  def update
+    respond_to do |format|
+      if @entry.update(entry_params)
+        format.html { redirect_to @entry, notice: 'Entry was successfully updated.' }
+        format.json { render :show, status: :ok, location: @entry }
+        format.js
+      else
+        format.html { render :edit }
+        format.json { render json: @entry.errors, status: :unprocessable_entity }
+        format.js   { render :edit }
+      end
+    end
+  end
+
+  # DELETE /entries/1
+  # DELETE /entries/1.json
+  def destroy
+    @entry.destroy
+    respond_to do |format|
+      format.html { redirect_to entries_url, notice: 'Entry was successfully destroyed.' }
+      format.json { head :no_content }
+      format.js   { render :edit }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_entry
+      @entry = Entry.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def entry_params
+      params.require(:entry).permit(:name, :address, :phone, :email)
+    end
+end
